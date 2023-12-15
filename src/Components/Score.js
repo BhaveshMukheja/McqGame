a948bcef02bbf1344e7d1be92ef20001b109a8c2
Bhavesh <122824805+BhaveshMukheja@users.noreply.github.com>
2023-12-15 22:56:27 +0530

    Update Score component

--- src/Components/Score.js
+++ src/Components/Score.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Score({ score, totalQuestions, restartQuiz }) {
+  
+  //returning the component front-end
+
   return (
     <>
       <div className="container mx-auto h-screen flex flex-col items-center justify-center bg-black">
@@ -21,7 +24,7 @@ function Score({ score, totalQuestions, restartQuiz }) {
         <div className="conatiner options flex flex-wrap w-1/2 justify-evenly items-center text-sky-100">
           <Link to="/quiz">
             <button
-              onClick={restartQuiz}
+              onClick={restartQuiz} //on clicking it sets the scroe to 0 again and retirect to the landing page
               type="button"
               className=" text-base font-bold mt-20 py-3 w-64 text-green-700  hover:text-white border border-green-700 hover:bg-green-500 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-500 dark:focus:ring-green-800 transition ease-in-out  hover:-translate-y-0.5 hover:scale-110  duration-300"
             >
@@ -30,7 +33,7 @@ function Score({ score, totalQuestions, restartQuiz }) {
           </Link>
           <Link to="/">
             <button
-              onClick={restartQuiz}
+              onClick={restartQuiz} //on clicking it sets the scroe to 0 again and retirect to the first question
               type="button"
               className=" text-base font-bold mt-20 py-3 w-64 text-sky-700  hover:text-white border border-sky-700 hover:bg-sky-500 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-sky-500 dark:text-sky-300 dark:hover:text-white-700 dark:hover:bg-sky-500 dark:focus:ring-sky-800 transition ease-in-out  hover:-translate-y-0.5 hover:scale-110  duration-300 "
             >
@@ -39,12 +42,6 @@ function Score({ score, totalQuestions, restartQuiz }) {
           </Link>
         </div>
       </div>
-      text-base font-bold py-3 w-64 text-green-700 hover:text-white border
-      border-green-700 hover:bg-green-500 focus:ring-4 focus:outline-none
-      focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5
-      text-center me-2 mb-2 dark:border-green-500 dark:text-green-500
-      dark:hover:text-white dark:hover:bg-green-500 dark:focus:ring-green-800
-      transition ease-in-out hover:-translate-y-0.5 hover:scale-110 duration-300
     </>
   );
 }
