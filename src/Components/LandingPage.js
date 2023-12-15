d70aeb5a3d388a075a50d9a9df92f08a9b5f321a
Bhavesh <122824805+BhaveshMukheja@users.noreply.github.com>
2023-12-15 22:58:19 +0530

    Update LandingPage

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import "../index.css";
 
 function LandingPage() {
+  //returning component front-end
+
   return (
     <>
       <div className="container mx-auto h-screen flex flex-col items-center justify-center bg-black">
@@ -13,12 +15,12 @@ function LandingPage() {
           </h2>
         </div>
 
+        {/* button to start the quiz and redirect to /quiz */}
         <Link to="/quiz">
           <button
             type="button"
             className=" text-base font-bold py-3 w-64 text-green-700  hover:text-white border border-green-700 hover:bg-green-500 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-500 dark:focus:ring-green-800 transition ease-in-out  hover:-translate-y-0.5 hover:scale-110  duration-300 "
           >
-            {" "}
             Start Quiz
           </button>
         </Link>
